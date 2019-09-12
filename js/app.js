8fab6dfbb86f789da0246539376c5ca4e699d45b
Matthew (School) <mweissman@horizon.csueastbay.edu>
2019-09-12 16:36:15 -0700

    Finished!

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 $(document).foundation();
 
-if (new Date().getDay() == 4)
+if (new Date().getDay() == 6)
 {
-  //document.getElementById("newProj").style.textShadow = 0 0 11px "red";
+  document.getElementById("newProj").style.textShadow = "0 0 11px rgb(255, 80, 10)";
+  document.getElementById("newProj").style.color = "rgb(255, 205, 50)";
 
-  document.getElementById("newProj").style.color = "red";
-  document.getElementById("newProj").style.color = "yellow";
+  document.getElementById("voss").style.textShadow = "0 0 11px rgb(255, 210, 90)";
+  document.getElementById("voss").style.color = "rgb(255, 230, 170)";
+
+  document.getElementById("legacy").style.textShadow = "0 0 11px rgb(10, 150, 255)";
+  document.getElementById("legacy").style.color = "rgb(40, 220, 255)";
+
+  document.getElementById("techThis").style.textShadow = "0 0 11px rgb(255, 205, 130)";
+  document.getElementById("techThis").style.color = "rgb(255, 245, 235)";
+
+  document.getElementById("survivors").style.textShadow = "0 0 11px rgb(255, 105, 25)";
+  document.getElementById("survivors").style.color = "rgb(255, 245, 200)";
+
 }
