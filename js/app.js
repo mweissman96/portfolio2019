a015347f0f84c3b716abff22e5c4f1bedc3bc19f
Matthew (School) <mweissman@horizon.csueastbay.edu>
2019-09-12 16:12:05 -0700

    stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+$(document).foundation();
+
+if (new Date().getDay() == 4)
+{
+  //document.getElementById("newProj").style.textShadow = 0 0 11px "red";
+
+  document.getElementById("newProj").style.color = "red";
+  document.getElementById("newProj").style.color = "yellow";
+}
